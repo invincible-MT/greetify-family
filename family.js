02f2c13116b2104930bd20899dbf712bdb236ff7
invincible-MT <44863093+invincible-MT@users.noreply.github.com>
2018-11-09 03:21:45 +0530

    Add files via upload

---
+++ family.js
@@ -0,0 +1,54 @@
+$('#elephant2').hide();
+$('#lizard2').hide();
+$('#snake2').hide();
+$('#cats2').hide();
+$('#fish2').hide();
+$('#bird2').hide();
+
+
+$('#elephant').click(function(){
+  $('#elephant2').show();
+});
+$('#elephant2').click(function(){
+  $('#elephant2').hide();
+});
+
+
+$('#lizard').click(function(){
+  $('#lizard2').show();
+});
+$('#lizard2').click(function(){
+  $('#lizard2').hide();
+});
+
+
+$('#snake').click(function(){
+  $('#snake2').show();
+});
+$('#snake2').click(function(){
+  $('#snake2').hide();
+});
+
+
+$('#cats').click(function(){
+  $('#cats2').show();
+});
+$('#cats2').click(function(){
+  $('#cats2').hide();
+});
+
+$('#fish').click(function(){
+  $('#fish2').show();
+});
+$('#fish2').click(function(){
+  $('#fish2').hide();
+});
+
+$('#bird').click(function(){
+  $('#bird2').show();
+});
+$('#bird2').click(function(){
+  $('#bird2').hide();
+});
+
+
